f57d836a322d3c456588e3fa4a4061d244ddb2ed
Eugenia Kutsel <jenna.kutsel@gmail.com>
2022-11-20 18:01:11 +0200

    Add styles

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -1,6 +1,5 @@
 import ContactForm from "components/ContactForm/ContactForm";
 import ContactList from "components/ContactList/ContactList";
-import Container from "components/Container/Container";
 import Filter from "components/Filter/Filter";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
@@ -18,16 +17,15 @@ const Contacts = () => {
   }, [dispatch]);
 
   return (
-    <Container>
-      <h1>Phonebook</h1>
+    <>
+      <h1>Welcome to your personal phonebook ;)</h1>
       <ContactForm />
-      <h2>Contacts</h2>
       <Filter />
       {isLoading && !error && <b>Request in progress...</b>}
       {contacts.length === 0 ?
         <p>Sorry, there are no contacts in the phonebook yet.</p> :
         <ContactList />}
-    </Container>
+    </>
   );
 }
 
