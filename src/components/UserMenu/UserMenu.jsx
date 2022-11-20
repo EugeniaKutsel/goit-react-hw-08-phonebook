70a1cf94a0756c2b15227a629ac6f98ebe66f638
Eugenia Kutsel <jenna.kutsel@gmail.com>
2022-11-20 03:02:21 +0200

    Update

--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -1,13 +1,11 @@
-import { useDispatch, useSelector} from "react-redux";
+import { useDispatch} from "react-redux";
 import { logOutUser } from "redux/auth/authOperations";
-import { userSelector } from "redux/auth/authSelectors";
 
 const UserMenu = () => {
   const dispatch = useDispatch();
-  const user = useSelector(userSelector);
   return (
     <div>
-      <p>Welcome, {user.name} </p>
+      <p>Welcome, </p>
       <button onClick={() => dispatch(logOutUser())}>Log out</button>
     </div>
   );
